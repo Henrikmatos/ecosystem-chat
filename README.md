e78df564b765147e48cf5e36de828c87667bbd57
Marcelo Vanderlinde Júnior <mvanderlindejr@gmail.com>
2024-02-08 23:03:37 -0400

    Primeira implantação

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # ecosystem-chat
 Implantação Traefik + Portainer + Postgres + Minio + Redis + Chatwoot + N8N + Evolution API (White Label)
+
+
+# Instalação Automatizada da Infraestrutura Docker
+
+Este guia fornece instruções para usar um script Bash que automatiza a instalação e configuração de uma infraestrutura Docker em um servidor Ubuntu 22.04. O script configura o ambiente para executar Docker, Docker Swarm, Traefik, Portainer e PostgreSQL.
+
+## Pré-requisitos
+
+- Ubuntu 22.04
+- Acesso root ou privilégios sudo
+- Conexão com a internet
+
+## Uso
+
+1. Clone o repositório ou baixe o script `implantacao.sh` para o seu servidor.
+2. Certifique-se de que o script é executável: `chmod +x implantacao.sh`
+3. Execute o script como root ou com sudo: `sudo ./implantacao.sh`
+
+
+
+
+## Passos Implementados no Script
+
+1. **Atualização do Sistema e Instalação do apparmor-utils**
+- Atualiza a lista de pacotes do sistema.
+- Instala o `apparmor-utils`.
+
+2. **Configuração do Hostname**
+- Define o hostname do servidor com base em uma variável definida no arquivo `.env`.
+
+3. **Atualização do Arquivo /etc/hosts**
+- Modifica o arquivo `/etc/hosts`, substituindo `127.0.0.1 localhost` por `127.0.0.1 NOME_SERVIDOR`.
+
+4. **Instalação do Docker**
+- Instala a versão mais recente do Docker utilizando o script de instalação disponibilizado pelo Docker.
+
+5. **Inicialização do Docker Swarm**
+- Inicializa o Docker em modo swarm.
+
+6. **Criação de Rede Overlay**
+- Cria uma rede overlay no Docker Swarm com base em uma variável definida no arquivo `.env`.
+
+7. **Deploy da Stack Traefik**
+- Realiza o deploy da stack Traefik utilizando o arquivo `traefik.yaml`.
+
+8. **Deploy da Stack Portainer**
+- Realiza o deploy da stack Portainer utilizando o arquivo `portainer.yaml`.
+
+9. **Deploy da Stack PostgreSQL**
+- Realiza o deploy da stack PostgreSQL utilizando o arquivo `postgres.yaml`.
+
+## Customização
+
+Para ajustar o script às suas necessidades específicas, você pode editar o arquivo `.env` para definir variáveis como `NOME_SERVIDOR` e `MINHA_REDE`, ou modificar os arquivos YAML (`traefik.yaml`, `portainer.yaml`, `postgres.yaml`) conforme necessário.
+
+## Contribuições
+
+Contribuições para o script são bem-vindas. Por favor, envie um pull request ou abra uma issue para discutir possíveis melhorias.
+
+## Licença
+
+Este script é fornecido sob a licença MIT. Veja o arquivo `LICENSE` para mais detalhes.
--- implantacao.sh
+++ implantacao.sh
@@ -50,6 +50,127 @@ sudo docker stack deploy --prune --resolve-image always -c portainer.yaml portai
 echo "Fazendo deploy da stack PostgreSQL..."
 sudo docker stack deploy --prune --resolve-image always -c postgres.yaml postgres
 
+echo ====================================================================================
+echo "AGUARDANDO CONTAINER POSTGRES"
+echo ====================================================================================
+# Define o nome ou parte do nome do container para busca
+CONTAINER_NAME_POSTGRES="postgres"
+
+# Tempo de espera entre as verificações (em segundos)
+SLEEP_TIME=10
+
+# Tempo máximo de espera para o container subir (em segundos)
+MAX_WAIT=300
+
+# Inicia contador de tempo
+start_time=$(date +%s)
+
+echo "Aguardando o container $CONTAINER_NAME_POSTGRES estar pronto..."
+
+while :; do
+    # Obtém o ID do container baseado no nome
+    POSTGRES_CONTAINER=$(docker ps --filter name=$CONTAINER_NAME_POSTGRES -q)
+
+    # Verifica se o container foi encontrado
+    if [ ! -z "$POSTGRES_CONTAINER" ]; then
+        echo "Container $CONTAINER_NAME_POSTGRES encontrado. Prosseguindo com a configuração..."
+        break
+    else
+        current_time=$(date +%s)
+        elapsed_time=$((current_time - start_time))
+
+        # Verifica se o tempo máximo de espera foi excedido
+        if [ $elapsed_time -ge $MAX_WAIT ]; then
+            echo "Tempo máximo de espera excedido. O container $CONTAINER_NAME_POSTGRES não está disponível."
+            exit 1
+        fi
+
+        # Espera por um período antes da próxima verificação
+        echo "Container $CONTAINER_NAME_POSTGRES ainda não está pronto. Aguardando..."
+        sleep $SLEEP_TIME
+    fi
+done
+
+echo ====================================================================================
+# Executa os comandos SQL para criar os bancos de dados
+echo "Criando bancos de dados..."
+docker exec -i $POSTGRES_CONTAINER psql -U postgres <<EOF
+CREATE DATABASE $DATABASE_CHATWOOT;
+CREATE DATABASE $DATABASE_N8N;
+EOF
+
+echo "Bancos de dados criados com sucesso."
+
+# Faz o deploy da stack MINIO com o arquivo minio.yaml
+echo "Fazendo deploy da stack MINIO..."
+sudo docker stack deploy --prune --resolve-image always -c minio.yaml minio
+
+# Faz o deploy da stack REDIS com o arquivo redis.yaml
+echo "Fazendo deploy da stack REDIS..."
+sudo docker stack deploy --prune --resolve-image always -c redis.yaml redis
+
+# Faz o deploy da stack N8N com o arquivo n8n.yaml
+echo "Fazendo deploy da stack N8N..."
+sudo docker stack deploy --prune --resolve-image always -c n8n.yaml n8n
+
+# Faz o deploy da stack CHATWOOT com o arquivo chatwoot.yaml
+echo "Fazendo deploy da stack CHATWOOT..."
+sudo docker stack deploy --prune --resolve-image always -c chatwoot.yaml chatwoot
+
+echo ====================================================================================
+echo "AGUARDANDO CONTAINER CHATWOOT"
+echo ====================================================================================
+# Define o nome ou parte do nome do container para busca
+CONTAINER_NAME="chatwoot_app"
+
+# Tempo de espera entre as verificações (em segundos)
+SLEEP_TIME=10
+
+# Tempo máximo de espera para o container subir (em segundos)
+MAX_WAIT=300
+
+# Inicia contador de tempo
+start_time=$(date +%s)
+
+echo "Aguardando o container $CONTAINER_NAME estar pronto..."
+
+while :; do
+    # Obtém o ID do container baseado no nome
+    CHATWOOT_CONTAINER=$(docker ps --filter name=$CONTAINER_NAME -q)
+
+    # Verifica se o container foi encontrado
+    if [ ! -z "$CHATWOOT_CONTAINER" ]; then
+        echo "Container $CONTAINER_NAME encontrado. Prosseguindo com a configuração..."
+        break
+    else
+        current_time=$(date +%s)
+        elapsed_time=$((current_time - start_time))
+
+        # Verifica se o tempo máximo de espera foi excedido
+        if [ $elapsed_time -ge $MAX_WAIT ]; then
+            echo "Tempo máximo de espera excedido. O container $CONTAINER_NAME não está disponível."
+            exit 1
+        fi
+
+        # Espera por um período antes da próxima verificação
+        echo "Container $CONTAINER_NAME ainda não está pronto. Aguardando..."
+        sleep $SLEEP_TIME
+    fi
+done
+echo ====================================================================================
+
+
+
+# Acessa o console do container e executa o comando para preparar o banco de dados
+echo "Preparando o banco de dados do Chatwoot..."
+docker exec -it $CHATWOOT_CONTAINER /bin/ash -c "bundle exec rails db:chatwoot_prepare"
+
+echo "Banco de dados do Chatwoot preparado com sucesso."
+
+
+
+
+
 # Adicione aqui mais comandos conforme necessário para configurar sua infraestrutura
 
 echo "Instalação concluída com sucesso."
